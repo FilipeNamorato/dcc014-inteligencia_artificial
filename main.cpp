6b3873855b3ec8304ba231f720b418d5e3111ec6
FilipeNamorato <filipennamorato@gmail.com>
2024-09-10 13:27:16 -0300

    update qtds de chamadas de cada método

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include "Grafo.h"
 #include "busca.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 using  std::string;
@@ -14,9 +15,9 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[]) {
     // Criar o grafo usando a função cria_grafo
-    int tipo_grafo = 1; // ou "denso" ou "esparso"
+    int tipo_grafo = 2; // ou "denso" ou "esparso"
     
-    Grafo *grafo_cidades = cria_grafo(1);
+    Grafo *grafo_cidades = cria_grafo(tipo_grafo);
 
     if (grafo_cidades == nullptr) {
         cerr << "Erro ao criar o grafo!" << endl;
@@ -42,79 +43,130 @@ int main(int argc, char *argv[]) {
     cout << "Sua escolha: ";
     std::cin >> opcao;
 
+    int numExecs = 5;
+
     if (opcao == 1) {
         // Executar o Backtracking
-        auto inicio = high_resolution_clock::now();
-
-        if (backtracking(grafo_cidades, cidade_origem, cidade_destino, caminho)) { 
-            cout << "\nCaminho encontrado" << endl;
-        } else {
-            cout << "\nNão existe caminho entre as cidades (usando Backtracking)." << endl;
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (backtracking(grafo_cidades, cidade_origem, cidade_destino, caminho)) {
+                cout << "\nCaminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (usando Backtracking)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
         }
 
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Backtracking): " << tempo_medio << " microssegundos" << endl;
 
     }else if (opcao == 2) {
-        // Executar a  em Largura
-        auto inicio = high_resolution_clock::now();
-        if (buscaLargura(grafo_cidades, cidade_origem, cidade_destino)) 
-            cout << "\nCaminho encontrado" <<endl;
-        else 
-            cout << "\nNão existe caminho entre as cidades (usando Busca em Largura)." <<endl;
+        // Executar a busca em Largura
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (buscaLargura(grafo_cidades, cidade_origem, cidade_destino)) {
+                cout << "\nCaminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (usando Busca em Largura)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
+        }
 
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Busca em Largura): " << tempo_medio << " microssegundos" << endl;
 
-    } else if (opcao == 3) {
+  } else if (opcao == 3) {
         // Executar a busca em Profundidade Limitada
-        auto inicio = high_resolution_clock::now();
-        if (buscaProfundidade(grafo_cidades, cidade_origem, cidade_destino, caminho, 0, limite_profundidade)) 
-            cout << "\nCaminho encontrado" <<endl;
-        else 
-            cout << "\nNão existe caminho entre as cidades (ou o limite de profundidade foi atingido)." <<endl;
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (buscaProfundidade(grafo_cidades, cidade_origem, cidade_destino, caminho, 0, limite_profundidade)) {
+                cout << "\nCaminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (ou o limite de profundidade foi atingido)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
+        }
+
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Busca em Profundidade Limitada): " << tempo_medio << " microssegundos" << endl;
 
     } else if (opcao == 4) {
         // Executar a busca Gulosa
-        auto inicio = high_resolution_clock::now();
-        if (buscaGulosa(grafo_cidades, cidade_origem, cidade_destino)) 
-            cout << "\nCaminho encontrado" <<endl;
-        else 
-            cout << "\nNão existe caminho entre as cidades (usando Busca Gulosa)." <<endl;
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (buscaGulosa(grafo_cidades, cidade_origem, cidade_destino)) {
+                cout << "\nCaminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (usando Busca Gulosa)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
+        }
+
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Busca Gulosa): " << tempo_medio << " microssegundos" << endl;
 
     } else if (opcao == 5) {
         // Executar a busca A*
-        auto inicio = high_resolution_clock::now();
-        if (buscaAEstrela(grafo_cidades, cidade_origem, cidade_destino, custo_solucao_a_estrela))
-            cout << "\nCaminho encontrado" <<endl;
-        else 
-            cout << "\nNão existe caminho entre as cidades (usando Busca A*)." <<endl;
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (buscaAEstrela(grafo_cidades, cidade_origem, cidade_destino, custo_solucao_a_estrela)) {
+                cout << "\nCaminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (usando Busca A*)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
+        }
 
-    } else if (opcao == 6) {
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Busca A*): " << tempo_medio << " microssegundos" << endl;
+
+    }else if (opcao == 6){
         // Executar a busca IDA*
-        auto inicio = high_resolution_clock::now();
-        if(buscaIDAEstrela(grafo_cidades, cidade_origem, cidade_destino))
-            cout<< "Caminho econtrado"<<endl;
-        else
-            cout << "\nNão existe caminho entre as cidades (usando Busca IDA*)." <<endl;
-        auto fim = high_resolution_clock::now();
-        auto duracao = duration_cast<microseconds>(fim - inicio);
-        cout << "Tempo de execução: " << duracao.count() << " microssegundos" << endl;
+        vector<long long> tempos_execucao;
+        for (int i = 0; i < numExecs; ++i) {
+            auto inicio = high_resolution_clock::now();
+            if (buscaIDAEstrela(grafo_cidades, cidade_origem, cidade_destino)) {
+                cout << "Caminho encontrado" << endl;
+            } else {
+                cout << "\nNão existe caminho entre as cidades (usando Busca IDA*)." << endl;
+            }
+            auto fim = high_resolution_clock::now();
+            auto duracao = duration_cast<microseconds>(fim - inicio);
+            tempos_execucao.push_back(duracao.count());
+        }
 
-    }else 
-        cout << "Opção inválida!" <<endl;
-    
+        // Calcular e imprimir a média dos tempos de execução
+        long long tempo_total = accumulate(tempos_execucao.begin(), tempos_execucao.end(), 0LL);
+        double tempo_medio = (double)tempo_total / numExecs;
+        cout << "Tempo médio de execução (Busca IDA*): " << tempo_medio << " microssegundos" << endl;
+
+    }else
+        cout << "Opção inválida!" << endl;
 
     delete grafo_cidades;
 
